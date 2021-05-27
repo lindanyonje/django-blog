0557dd7d571605ed1fa2260e6546f7b32671e55a
STUDENT <lindaatieno24@gmail.com>
2021-05-27 13:36:23 +0300

    added a template response

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Category
 
 # Create your views here.
 
 def home(request):
 
-    return HttpResponse("Hello world")
+    #dictionary with initial data
+    #with field name as keys
+
+    context = {}
+
+    #add dictionary during initiliasation
+
+    context["dataset"]= Category.objects.all()
+    return render(request,"home.html",context)
